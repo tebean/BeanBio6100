9d4e0908bf1d0065c329ada55bcffbbb0517e6c3
tebean <tebean@uvm.edu>
2025-04-08 14:01:03 -0400

    files

---
+++ ggplot3a.R
@@ -0,0 +1,14 @@
+#TEB
+#April 8, 2025
+#ggplot 3a
+
+library(ggplot2)
+library(ggridges)
+library(ggbeeswarm)
+library(GGally)
+library(ggpie)
+library(ggmosaic)
+library(scatterpie)
+library(waffle)
+library(DescTools)
+library(treemap)
